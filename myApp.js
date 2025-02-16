d1f6b29177b1cb0d0c0417c0773a82acb130ccfc
nujiznaw8240 <nujiznaw8240@gmail.com>
2025-02-16 21:51:09 +0100

    modified: myApp.js

--- myApp.js
+++ myApp.js
@@ -1,30 +1,38 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
+console.log("Hello World");
 
+app.use(function(req, res, next) {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+});
 
+app.use('/public', express.static(__dirname + '/public'));
 
+app.get("/json", (req, res) => {
+    console.log(process.env.MESSAGE_STYLE);
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        res.json({
+            "message": "HELLO JSON"
+        });
+    } else {
+        res.json({
+            "message": "Hello json"
+        });
+    }
+});
 
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function(req, res) {
+    res.json({time: req.time});
+});
 
 
 
